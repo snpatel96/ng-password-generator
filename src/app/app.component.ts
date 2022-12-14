26c0a74564c1273119cc47191e92fa513b7cc9eb
snehal patel <snpatel.dev@gmail.com>
2022-12-13 22:14:43 -0500

    #1 binding syntax, methods & HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  passwordLength = 0;
+  password = '';
+
+  onChangeLength(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    console.log(this.includeLetters);
+    this.password = 'my password';
+  }
 }
