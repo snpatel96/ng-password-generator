32b451b866144c7c73715d5910a2c322d51a5a15
snehal patel <snpatel.dev@gmail.com>
2022-12-16 17:29:43 -0500

    #2 logic and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,26 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(this.includeLetters);
-    this.password = 'my password';
+    const numbers = '1234567890';
+    const letters = 'qwertyuioplkjhgfdsazxcvbnm';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatePassword = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+    this.password = generatePassword;
   }
 }
